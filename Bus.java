3b23442f3c9c8af736e29805fcb4765c38322fda
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-10-08 21:46:57 -0400

    commoned horn

--- Bus.java
+++ Bus.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+    import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * The Bus subclass
@@ -17,7 +17,7 @@ public class Bus extends Vehicle
 
     /**
      * Act - do whatever the Bus wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * the 'Act' or 'Run' button gets pressed in the environment.   
      */
     public void act()
     {
--- Car.java
+++ Car.java
@@ -23,41 +23,41 @@ public class Car extends Vehicle
     }
 
 
-        public void honkHorn() {
-        try {
-            // Load the horn audio file
-            File hornAudioFile = new File("car_horn.wav"); // Replace with your horn audio file path
+        // public void honkHorn() {
+        // try {
+            // // Load the horn audio file
+            // File hornAudioFile = new File("carHorn.mp3"); // Replace with your horn audio file path
 
-            // Create an audio input stream for the horn
-            AudioInputStream hornAudioInputStream = AudioSystem.getAudioInputStream(hornAudioFile);
+            // // Create an audio input stream for the horn
+            // AudioInputStream hornAudioInputStream = AudioSystem.getAudioInputStream(hornAudioFile);
 
-            // Get the audio format for the horn
-            AudioFormat format = hornAudioInputStream.getFormat();
+            // // Get the audio format for the horn
+            // AudioFormat format = hornAudioInputStream.getFormat();
 
-            // Create a data line for horn audio playback
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            // // Create a data line for horn audio playback
+            // DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
 
-            // Open the data line for horn audio and start playback
-            SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
-            sourceDataLine.open(format);
-            sourceDataLine.start();
+            // // Open the data line for horn audio and start playback
+            // SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+            // sourceDataLine.open(format);
+            // sourceDataLine.start();
 
-            // Read and play the horn audio data
-            byte[] buffer = new byte[4096];
-            int bytesRead;
+            // // Read and play the horn audio data
+            // byte[] buffer = new byte[4096];
+            // int bytesRead;
 
-            while ((bytesRead = hornAudioInputStream.read(buffer)) != -1) {
-                sourceDataLine.write(buffer, 0, bytesRead);
-            }
+            // while ((bytesRead = hornAudioInputStream.read(buffer)) != -1) {
+                // sourceDataLine.write(buffer, 0, bytesRead);
+            // }
 
-            // Close the data line and horn audio input stream
-            sourceDataLine.drain();
-            sourceDataLine.close();
-            hornAudioInputStream.close();
-        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
+            // // Close the data line and horn audio input stream
+            // sourceDataLine.drain();
+            // sourceDataLine.close();
+            // hornAudioInputStream.close();
+        // } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+            // e.printStackTrace();
+        // }
+    // }
 
     /**
      * When a Car hit's a Pedestrian, it should knock it over and honk the horn
@@ -67,7 +67,7 @@ public class Car extends Vehicle
         if (p != null)
         {
             p.knockDown();
-            honkHorn(); // Call the honkHorn method when hitting a pedestrian
+            //honkHorn(); // Call the honkHorn method when hitting a pedestrian
             return true;
         }
         return false;
